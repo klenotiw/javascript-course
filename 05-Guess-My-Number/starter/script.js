05845d4252c88387d718c06f4641ba8041b0b9ee
Bill Klenotiz <klenotiw@gmail.com>
2021-09-13 12:11:44 -0400

    guess my number

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,39 @@
 'use strict';
+/* console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = 'Correct Number!';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 23; */
+
+let theNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector('.number').textContent = theNumber;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No Number!';
+  } else if (guess === theNumber) {
+    document.querySelector('.message').textContent = 'Correct Number!!';
+  } else if (guess < theNumber) {
+    if (score > 0) {
+      document.querySelector('.message').textContent = 'Too Low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You Lose!';
+    }
+  } else {
+    if (score > 0) {
+      document.querySelector('.message').textContent = 'Too High!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You Lose!';
+    }
+  }
+});
